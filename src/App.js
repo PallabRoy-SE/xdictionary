3befa282e95931e13269576fd1cd305bba11b3da
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-06-05 14:39:45 +0530

    feat: add dictionary functionalities

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const customDictionary = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [suggestedText, setSuggestedText] = useState("");
+
+  const handleSearch = () => {
+    if (inputText.length) {
+      const wordFound = customDictionary.find(
+        (item) =>
+          item.word.trim().toLowerCase() === inputText.trim().toLowerCase()
+      );
+      setSuggestedText(
+        wordFound?.meaning ?? "Word not found in the dictionary."
+      );
+    } else setSuggestedText("Word not found in the dictionary.");
+  };
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+      <input
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        type="text"
+        placeholder="Search for a word..."
+      />
+      <button onClick={() => handleSearch()}>Search</button>
+      <p>
+        <strong>Definition: </strong>
+      </p>
+
+      {suggestedText && <p>{suggestedText}</p>}
+    </div>
+  );
+}
+
+export default App;
